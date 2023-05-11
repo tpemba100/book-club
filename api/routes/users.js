9ead2b28bbb89abc59da98975b94d0a546334ff0
tpemba100 <tpemba.96@gmail.com>
2023-05-11 17:51:39 -0400

    bug fix.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJS = require("crypto-js");
+// const CryptoJS = require("crypto-js");
 
 //  POST "REGISTER"
 //        --> register new user
@@ -9,10 +9,11 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SECRET_KEY
-    ).toString(),
+    password: req.body.password,
+    // password: CryptoJS.AES.encrypt(
+    //   req.body.password,
+    //   process.env.SECRET_KEY
+    // ).toString(),
   });
   console.log("creating new user");
   console.log(newUser);
